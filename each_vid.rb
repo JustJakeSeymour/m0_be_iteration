0fa40d1f7d39bd6d9333171af8e7ff4bf6a85e7b
J Seymour <JustJakeSeymour@gmail.com>
2022-09-06 12:42:57 -0600

    each_vid.rb for live coding

---
+++ each_vid.rb
@@ -0,0 +1,15 @@
+# Challenge 2
+# Use the each method to print out the words that are exactly 3 characters in length
+words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
+## create each method
+words.each do |word|
+  if word.length == 3
+    then p word
+  end
+end
+
+# Record yourself live-coding and talking through either Challenge 1 or 2.
+# The recording doesn't have to be the first time you are solving the problem.
+# Use technical vocabulary and explain each part to demonstrate your understanding!
+# Share the link to your video in your small group channel.
